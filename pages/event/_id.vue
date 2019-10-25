c161727d0174333c0d4212851265c7c7d906d1d6
andriy <andriytupac@gmail.com>
2019-10-25 12:54:23 +0300

    error page and add routes

---
+++ pages/event/_id.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>Event #{{ id }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: 'Event #' + this.id,
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'What you nned to know about event #' + this.id
+        }
+      ]
+    }
+  },
+  computed: {
+    id() {
+      return this.$route.params.id ? this.$route.params.id : ''
+    }
+  }
+}
+</script>
+
+<style></style>
